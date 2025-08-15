81a09f498da6db4fa189f4107386936736a51c23
Kishore Kumar <Kishoreduraisamy@users.noreply.huggingface.co>
2025-08-15 19:57:08 +0000

    Update Gradio_UI.py
    Fixed another UI error

--- Gradio_UI.py
+++ Gradio_UI.py
@@ -144,7 +144,8 @@ def stream_to_gradio(
         if hasattr(agent.model, "last_input_token_count"):
             if agent.model.last_input_token_count is not None:
                 total_input_tokens += agent.model.last_input_token_count
-            total_output_tokens += agent.model.last_output_token_count
+            if agent.model.last_output_token_count is not None:
+                total_output_tokens += agent.model.last_output_token_count
             if isinstance(step_log, ActionStep):
                 step_log.input_token_count = agent.model.last_input_token_count
                 step_log.output_token_count = agent.model.last_output_token_count
